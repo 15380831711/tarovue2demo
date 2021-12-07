272386452e9599e01241ba023b3836305dd3217d
15380831711 <15380831711>
2021-12-07 17:22:39 +0800

    '初次见面'

---
+++ config/dev.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    NODE_ENV: '"development"',
+  },
+  defineConstants: {},
+  mini: {},
+  h5: {},
+  /* plugins: [
+    [
+      "taro-plugin-sync-in-wsl",
+      {
+        weapp: [
+          {
+            sourcePath: "dist",
+            outputPath: "/mnt/d/path/in/windows/Code/dist",
+          },
+          {
+            sourcePath: "cloud",
+            outputPath: "/mnt/d/path/in/windows/Code/cloud",
+          },
+        ],
+      },
+    ],
+  ], */
+};
---
+++ src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="index">
+    <input v-model="msg" placeholder="请输入">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import './index.scss'
+
+export default {
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
